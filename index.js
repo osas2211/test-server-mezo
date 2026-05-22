bd0f975ce79e39741b38b20d3fa39a55c6364c0b
Osaretin Frank <osaretin.frank10@gmail.com>
2026-05-22 14:00:48 +0100

    updates

---
+++ index.js
@@ -0,0 +1,19 @@
+require("dotenv").config();
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  const name = process.env.NAME;
+
+  if (!name) {
+    return res.status(500).json({ error: "NAME is not set in .env" });
+  }
+
+  res.json({ name });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
