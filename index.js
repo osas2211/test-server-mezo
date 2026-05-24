14a66d90f0ade45d391fda47f3b79bd47e5b8ee2
Osaretin Frank <osaretin.frank10@gmail.com>
2026-05-24 14:57:38 +0100

    chore

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-require("dotenv").config();
-const express = require("express");
+require("dotenv").config()
+const express = require("express")
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+const app = express()
+const PORT = process.env.PORT || 3000
 
-app.get("/", (req, res) => {
-  const name = process.env.NAME;
+app.get__("/", (req, res) => {
+  const name = process.env.NAME
 
   if (!name) {
-    return res.status(500).json({ error: "NAME is not set in .env" });
+    return res.status(500).json({ error: "NAME is not set in .env" })
   }
 
-  res.json({ name });
-});
+  res.json({ name })
+})
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+  console.log(`Server running at http://localhost:${PORT}`)
+})
